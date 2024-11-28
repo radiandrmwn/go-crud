37f2876ec8f60d147dcf1ab9e8538c1941fac784
radiandrmwn <radiantrydarmawan13@gmail.com>
2024-11-28 22:23:03 +0700

    first commit

---
+++ config/database.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+    "database/sql"
+    "log"
+
+    _ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func ConnectDB() {
+    dsn := "root:@tcp(localhost:3306)/sql_godev?parseTime=true"
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        log.Fatalf("Error opening database: %v", err)
+    }
+
+    if err := db.Ping(); err != nil {
+        log.Fatalf("Error connecting to the database: %v", err)
+    }
+
+    DB = db
+    log.Println("Database connected")
+}
